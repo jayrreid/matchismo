97e02fe55b0540b66bd904a782c68b7ff1e28fb2
James Reid <jay.r.reid@gmail.com>
2013-12-31 00:14:01 -0500

    added touchCardButton to CardGameViewController to control the flipping of the card.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -14,16 +14,19 @@
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (IBAction)touchCardButton:(UIButton *)sender {
+    
+    if([sender.currentTitle length]){
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                          forState:(UIControlStateNormal)];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                          forState:(UIControlStateNormal)];
+        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+    }
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
