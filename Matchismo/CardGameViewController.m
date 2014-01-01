cfc00818b953f6478f0ad38c196fcbd439182078
James Reid <jay.r.reid@gmail.com>
2013-12-31 22:48:25 -0500

    added the additional hints: make it come up showing the back of the card instead at the start and if every card in the deck has been shown it stops flipping.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -28,30 +28,45 @@
 {
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
-    NSLog(@"flipCount changed to %d", self.flipCount);
+    //NSLog(@"flipCount changed to %d", self.flipCount);
 }
 
 - (IBAction)touchCardButton:(UIButton *)sender {
     
+    // get deck of cards and get a random card
+    Deck* deck = self.deck;
+    Card* randomCard = [deck drawRandomCard];
     
-    if([sender.currentTitle length]){
+    // if random card not chosen and there are cards remaining then flip the card
+    if (randomCard != nil && ! [randomCard isChosen])
+    {
+        // set card to chosen
+        [randomCard setChosen:YES];
+        
+        // flip card
+        if([sender.currentTitle length]){
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                          forState:(UIControlStateNormal)];
+            [sender setTitle:@"" forState:UIControlStateNormal];
+        } else {
+        
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                          forState:(UIControlStateNormal)];
+        
+            // set title to random card's rank and suit
+            [sender setTitle:[randomCard contents]forState:UIControlStateNormal];
+        }
+        
+        // increment flip counter
+        self.flipCount++;
+    } else {
+        // No more cards to flip therefore show cardback
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                           forState:(UIControlStateNormal)];
         [sender setTitle:@"" forState:UIControlStateNormal];
-    } else {
-        
-        // get deck of cards and get a random card
-        Deck* deck = self.deck;
-        Card* randomCard = [deck drawRandomCard];
-
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:(UIControlStateNormal)];
-        
-        // set title to random card's rank and suit
-        [sender setTitle:[randomCard contents]forState:UIControlStateNormal];
     }
     
-    self.flipCount++;
+    
     
 }
 
